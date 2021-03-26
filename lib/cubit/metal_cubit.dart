edbadecd027709b87faf6f7ab240aa39930f67b6
hexaludira <hexaludira@gmail.com>
2021-03-26 16:14:50 +0700

    Lanjut_lagi

--- lib/cubit/metal_cubit.dart
+++ lib/cubit/metal_cubit.dart
@@ -11,11 +11,40 @@ class MetalCubit extends Cubit<MetalState> {
 	MetalCubit(this.dioHelper) : super(InitialMetalState());
 
 	//Cubit method untuk mendapatkan data list metal (GET)
-	void getAllProfiles() async {
+	void getAllData() async {
 		emit(LoadingMetalState());
 		var result = await dioHelper.getAllData();
-		result.fold()
+		result.fold(
+			(errorMessage) => emit(FailureLoadAllMetalState(errorMessage)),
+			(listMetal) => emit(SuccessLoadAllMetalState(listMetal)),
+		);
 	}
 
+	//Cubit method untuk menambahkan data list metal (POST)
+	void addData(MetalData metalData) async{
+		emit(LoadingMetalState());
+		var result = await dioHelper.addData(metalData);
+		result.fold(
+			(errorMessage) => emit(FailureSubmitMetalState(errorMessage)),
+			(_) => emit(SuccessSubmitMetalState()),
+		);
+	}
 
+	//Cubit method untuk mengupdate data list metal (PUT)
+	void editData(MetalData metalData) async {
+		emit(LoadingMetalState());
+		var result = await dioHelper.editData(metalData);
+		result.fold(
+			(errorMessage) => emit(FailureSubmitMetalState(errorMessage)),
+			(_) => emit(SuccessSubmitMetalState()),
+		);
+	}
+
+	//Cubit method untuk menghapus data list metal (DELETE)
+	void deleteData(int id) async {
+		emit(LoadingMetalState());
+
+		var resultDelete = await dioHelper.deleteData(id);
+		
+	}
 }
